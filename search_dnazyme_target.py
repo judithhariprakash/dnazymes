10a59d125ba0253a0a6daf61375597268dc43d09
judithhariprakash <judithharipraksh@gmail.com>
2015-09-03 05:13:04 +0000

    Made DNAzymes for GU region as well

--- search_dnazyme_target.py
+++ search_dnazyme_target.py
@@ -4,16 +4,19 @@ from Bio.Seq import Seq
 with open("merged_backsplice_sequences15bp.json") as handle:
     data = json.load(handle)
 
-
 def locus_find():
     locus = {}
+    locusgu = {}
     for cid in data:
-        for x in range(10,20):
-            if data[cid][x] == "A":
-                if data[cid][x+1] == "G":
-                    locus[cid] = x
-            x+=1
-    return locus
+        #for x in range(10,20):
+            #if data[cid][x] == "A":
+                #if data[cid][x+1] == "G":
+                    #locus[cid] = x
+        for y in range(10,20):
+            if data[cid][y] == "G":
+                if data[cid][y+1] == "T":
+                    locusgu[cid] = y
+    return locusgu
     #split_seq(locus)
 
 #def split_seq(locus):
@@ -46,11 +49,14 @@ def dnazyme_creater(locus):
 
 
 if __name__ == '__main__':
-    loci_lib = locus_find()
-    dnazyme_lib = dnazyme_creater(loci_lib)
-    print len(dnazyme_lib)
-    with open ('dnazyme_sequences.json','w') as handle:
-        json.dump(dnazyme_lib, handle, indent = 2)
+    #loci_lib,
+    locigu = locus_find()
+    #dnazyme_lib = dnazyme_creater(loci_lib)
+    dzyme_gu = dnazyme_creater(locigu)
+    #print len(dzyme_gu)
+    print len(dzyme_gu)
+    with open ('dnazyme_sequences_GU.json','w') as handle:
+        json.dump(dzyme_gu, handle, indent=2)
 
 
 
